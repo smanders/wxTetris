7d0e2c32d215f5d535b392254b6005b3e23f77f1
Scott M Anderson <smanders@sdl.usu.edu>
2018-02-12 15:17:12 -0700

    update to externpro 18.01.1

--- Findexternpro.cmake
+++ Findexternpro.cmake
@@ -4,6 +4,8 @@
 # should match xpGetCompilerPrefix in externpro's xpfunmac.cmake
 # NOTE: wanted to use externpro version, but chicken-egg problem
 function(getCompilerPrefix _ret)
+  set(options GCC_TWO_VER)
+  cmake_parse_arguments(X "${options}" "" "" ${ARGN})
   if(MSVC)
     if(MSVC14)
       set(prefix vc140)
@@ -28,13 +30,18 @@ function(getCompilerPrefix _ret)
     endif()
   elseif(CMAKE_COMPILER_IS_GNUCXX)
     exec_program(${CMAKE_CXX_COMPILER}
-      ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion
+      ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpfullversion -dumpversion
       OUTPUT_VARIABLE GCC_VERSION
       )
-    string(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+)?" "\\1\\2\\3"
-      GCC_VERSION ${GCC_VERSION}
+    if(X_GCC_TWO_VER)
+      set(digits "\\1\\2")
+    else()
+      set(digits "\\1\\2\\3")
+    endif()
+    string(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+)?"
+      "gcc${digits}"
+      prefix ${GCC_VERSION}
       )
-    set(prefix gcc${GCC_VERSION})
   elseif(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang") # LLVM/Apple Clang (clang.llvm.org)
     if(${CMAKE_SYSTEM_NAME} STREQUAL Darwin)
       exec_program(${CMAKE_CXX_COMPILER}
@@ -84,13 +91,13 @@ find_path(externpro_DIR
     C:/src/externpro/_bld/externpro_${externpro_SIG}
     ~/src/externpro/_bld/externpro_${externpro_SIG}
     # environment variable
-    "$ENV{externpro_DIR}/externpro ${externpro_SIG}"
-    "$ENV{externpro_DIR}/externpro-${externpro_SIG}-${CMAKE_SYSTEM_NAME}"
+    "$ENV{extern_DIR}/externpro ${externpro_SIG}"
+    "$ENV{extern_DIR}/externpro-${externpro_SIG}-${CMAKE_SYSTEM_NAME}"
     # installed versions
     "$ENV{ProgramW6432}/externpro ${externpro_SIG}"
     "$ENV{${PFX86}}/externpro ${externpro_SIG}"
-    "~/externpro/externpro-${externpro_SIG}-${CMAKE_SYSTEM_NAME}"
-    "/opt/externpro/externpro-${externpro_SIG}-${CMAKE_SYSTEM_NAME}"
+    "~/extern/externpro-${externpro_SIG}-${CMAKE_SYSTEM_NAME}"
+    "/opt/extern/externpro-${externpro_SIG}-${CMAKE_SYSTEM_NAME}"
   DOC "externpro directory"
   )
 if(NOT externpro_DIR)

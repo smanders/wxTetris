641a3ebee2df01391c6220a42bf4940d4c391b7d
Scott M Anderson <smanders@sdl.usu.edu>
2021-12-23 09:50:39 -0700

    dummy.cpp for checking wxx includes

---
+++ src/dummy.cpp
@@ -0,0 +1,6 @@
+//#include <wx/things/bmpcombo.h>
+//#include <wx/things/dropdown.h>
+//#include <wx/things/optvalue.h>
+//#include <wx/things/range.h>
+//#include <wx/things/toggle.h>
+//#include <wx/plotctrl/plotdraw.h>
